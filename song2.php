b138e27a8aebf59aca2e5ee376178e9ba14ff46e
Jhonas Lamar <jhonaslamarck6@gmail.com>
2024-03-11 18:23:53 +0800

    song 2 commited by Jhonas Lamar

---
+++ song2.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Paraluman</title>
+</head>
+<body>
+    <h2>Crazier</h2>
+    <h4>Artist: Taylor Swift</h4>
+
+    <?php
+       echo" Lyrics: <br><br>";
+       echo"I'd never gone with the wind, just let it flow
+       <br>";
+       echo"Let it take me where it wants to go
+       <br>";
+       echo"'Til you opened the door
+       <br>";
+       echo"And there's so much more
+       <br>";
+       echo" I'd never seen it before<br>";
+       echo"I was trying to fly, but I couldn't find wings
+       <br>";
+       echo"But you came along and you changed everything
+       <br>";
+       echo" <br>";
+       echo"You lift my feet off the ground
+       <br>";
+       echo"You spin me around <br>";
+       echo"You make me crazier, crazier
+       <br>";
+       echo"Feels like I'm falling and I
+       <br>";
+       echo"I'm lost in your eyes
+       <br>";
+       echo"You make me crazier, crazier, crazier
+       <br>";
+       echo" <br>";
+       echo"I watched from a distance as you
+       <br>";
+       echo"Made life your own
+       <br>";
+       echo"Every sky was your own kind of blue <br>";
+       echo"And I wanted to know how that would feel
+       <br>";
+       echo"And you made it so real
+       <br>";
+       echo"You showed me something that I couldn't see
+       <br>";
+       echo"You opened my eyes and you made me believe <br>";
+       echo" <br>";
+       echo"You lift my feet off the ground <br>";
+       echo"You spin me around <br>";
+       echo"You make me crazier, crazier
+       <br>";
+       echo"Feels like I'm falling and I
+       <br>";
+       echo"I'm lost in your eyes
+       <br>";
+       echo"You make me crazier, crazier, crazier, oh-oh
+       <br>";
+       echo" <br>";
+       echo"Baby, you showed me what living is for <br>";
+       echo"I don't wanna hide anymore, oh-oh
+       <br>";
+       echo" <br>";
+       echo"You lift my feet off the ground
+       <br>";
+       echo"You spin me around
+       <br>";
+       echo"You make me crazier, crazier
+       <br>";
+       echo"Feels like I'm falling and I
+       <br>";
+       echo"I'm lost in your eyes
+       <br>";
+       echo"You make me crazier, crazier, crazier
+       <br>";
+       echo" <br>";
+       echo"Crazier, crazier...
+       <br>";
+
+    ?>
+
+    <button><a href="index.php">Back</a></button>
+
+</body>
+
+   <style>
+       body{
+        text-align: center;
+       }
+       button a{
+        text-decoration: none;
+        border-radius: 5px;
+       }
+   </style>
+</html>
