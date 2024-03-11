e8f4f2d35a8d916d3d5b40cc9ac6878e290c3c5b
Jhonas Lamar <jhonaslamarck6@gmail.com>
2024-03-11 18:21:10 +0800

    index commited by Jhonas Lamar

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+</head>
+<body>
+    <h1>Song Book</h1>
+    <ol>
+        <li><a href="song1.php">Paraluman</a></li>
+        <li><a href="song2.php">Crazier</a></li>
+        <li><a href="song3.php">What a Wonderful World</a></li>
+        <li><a href="song4.php">Pasilyo</a></li>
+        <li><a href="song5.php">Raindrops</a></li>
+    </ol> 
+</body>
+</html>
