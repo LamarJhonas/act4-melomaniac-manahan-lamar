9f1738719c6a661e130ebb13b87228605e610d3d
Jhonas Lamar <jhonaslamarck6@gmail.com>
2024-03-11 18:25:36 +0800

    song 4 commited by Jhonas Lamar

---
+++ song4.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pasilyo</title>
+</head>
+<body>
+    <h2>Pasilyo</h2>
+    <h4>Artist: Sunkissed Lola</h4>
+
+    <?php
+       echo" Lyrics: <br><br>";
+       echo"Palad ay basang-basa
+       <br>";
+       echo"Ang dagitab ay damang-dama
+       <br>";
+       echo"Sa 'king kalamnang punong-puno
+       <br>";
+       echo"Ng pananabik at ng kaba
+       <br>";
+       echo"Lalim sa 'king bawat paghinga
+       <br>";
+       echo"Nakatitig lamang sa iyo
+       <br>";
+       echo" <br>";
+       echo"Naglakad ka nang dahan-dahan
+       <br>";
+       echo"Sa pasilyo tungo sa altar ng simbahan
+       <br>";
+       echo"Hahagkan na't 'di ka bibitawan
+       <br>";
+       echo"Wala na 'kong mahihiling pa
+       <br>";
+       echo" <br>";
+       echo"Ikaw at ikaw
+       <br>";
+       echo"Ikaw at ikaw
+       <br>";
+       echo"Ikaw at ikaw
+       <br>";
+       echo"Ikaw at ikaw
+       <br>";
+       echo" <br>";
+       echo"'Di maikukumpara
+       <br>";
+       echo"Araw-araw kong dala-dala
+       <br>";
+       echo"Paboritong panalangin ko'y
+       <br>";
+       echo"Makasama ka sa pagtanda
+       <br>";
+       echo"Ang hiling sa Diyos na may gawa
+       <br>";
+       echo"Apelyido ko'y maging iyo
+       <br>";
+       echo" <br>";
+       echo"Naglakad ka nang dahan-dahan
+       <br>";
+       echo"Sa pasilyo tungo sa 'kin
+       <br>";
+       echo"At hinawakan mo ako't aking 'di napigilang
+       <br>";
+       echo"Maluha nang mayakap na
+       <br>";
+       echo" <br>";
+       echo"Ikaw at ikaw
+       <br>";
+       echo"Ikaw at ikaw
+       <br>";
+       echo"Ikaw at ikaw
+       <br>";
+       echo"Ikaw at ikaw
+       <br>";
+       echo" <br>";
+       echo"Ikaw at ikaw (ikaw at ikaw, ikaw at ikaw)
+       <br>";
+       echo"Ikaw at ikaw (ikaw at ikaw, ikaw at ikaw)
+       <br>";
+       echo"Ikaw at ikaw (ikaw at ikaw, ikaw at ikaw)
+       <br>";
+       echo"Ikaw at ikaw (ikaw at ikaw, ikaw at ikaw)
+       <br>";
+       echo" <br>";
+       echo"palad ay basang-basa, ang dagitab ay damang-dama
+       <br>";
+       echo"(Ikaw at ikaw) sa 'king kalamnang punong-puno
+       <br>";
+       echo"(Ikaw at ikaw) 'di maikukumpara, araw-araw kong dala-dala
+       <br>";
+       echo"(Ikaw at ikaw) paboritong panalangin ko'y ikaw
+       <br>";
+
+
+    ?>
+
+    <button><a href="index.php">Back</a></button>
+
+</body>
+
+   <style>
+       body{
+        text-align: center;
+       }
+       button a{
+        text-decoration: none;
+        border-radius: 5px;
+       }
+   </style>
+</html>
