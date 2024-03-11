6ff00c54dcc4b28c7bd87be40b83628ae75dcc37
Jhonas Lamar <jhonaslamarck6@gmail.com>
2024-03-11 18:24:43 +0800

    song 3 commited by Jhonas Lamar

---
+++ song3.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>What a Wonderful World</title>
+</head>
+<body>
+    <h2>What a Wonderful World</h2>
+    <h4>Artist: Louis Armstrong</h4>
+
+    <?php
+       echo" Lyrics: <br><br>";
+       echo"I see trees of green
+       <br>";
+       echo"Red roses too
+       <br>";
+       echo"I see them bloom
+       <br>";
+       echo"For me and you
+       <br>";
+       echo"And I think to myself
+       <br>";
+       echo"What a wonderful world
+       <br>";
+       echo" <br>";
+       echo"I see skies of blue <br>";
+       echo"And clouds of white
+       <br>";
+       echo"The bright blessed day
+       <br>";
+       echo"The dark sacred night
+       <br>";
+       echo"And I think to myself
+       <br>";
+       echo"What a wonderful world
+       <br>";
+       echo" <br>";
+       echo"The colors of the rainbow
+       <br>";
+       echo"So pretty in the sky
+       <br>";
+       echo"Are also on the faces
+       <br>";
+       echo"Of people going by
+       <br>";
+       echo"I see friends shaking hands
+       <br>";
+       echo"Saying, How do you do?
+       <br>";
+       echo"They're really saying
+       <br>";
+       echo"I love you
+       <br>";
+       echo" <br>";
+       echo"I hear babies cry
+       <br>";
+       echo"I watch them grow
+       <br>";
+       echo"They'll learn much more
+       <br>";
+       echo"Than I'll ever know
+       <br>";
+       echo"And I think to myself
+       <br>";
+       echo"What a wonderful world
+       <br>";
+       echo"Yes, I think to myself
+       <br>";
+       echo"What a wonderful world
+       <br>";
+       echo"Ooh, yes
+       <br>";
+    ?>
+
+    <button><a href="index.php">Back</a></button>
+
+</body>
+
+   <style>
+       body{
+        text-align: center;
+       }
+       button a{
+        text-decoration: none;
+        border-radius: 5px;
+       }
+   </style>
+</html>
