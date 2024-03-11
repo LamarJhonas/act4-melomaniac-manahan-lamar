b2014cd490ca140f85c77b23d8e3ed3e4589d201
Jhonas Lamar <jhonaslamarck6@gmail.com>
2024-03-11 18:22:49 +0800

    song 1 commited  by Jhonas Lamar

---
+++ song1.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Paraluman</title>
+</head>
+<body>
+    <h2>Paraluman</h2>
+    <h4>Artist: Addie</h4>
+
+    <?php
+       echo" Lyrics: <br><br>";
+       echo"Sa unang tingin, agad na nahumaling <br>";
+       echo"Sa nagniningning mong mga mata <br>";
+       echo"Ika'y isang bituin na nagmula sa langit <br>";
+       echo"<br>";
+       echo"Hindi ko mawari ang taglay mong tinatangi <br>";
+       echo"Sadya namang nakakabighani <br>";
+       echo"'Di maipaliwanag ang nararamdaman<br>";
+       echo" <br>";
+       echo"Namumukadkad ang aking ligaya <br>";
+       echo"Sa tuwing ika'y papalapit na <br>";
+       echo"Hawakan mo ang aking kamay <br>";
+       echo" <br>";
+       echo"Oh, Paraluman, ika'y akin nang <br>";
+       echo"Dadalhin sa 'di mo inaasahang paraiso
+       <br>";
+       echo"Palagi kitang aawitan ng Kundiman <br>";
+       echo"Di magsasawa, 'di ka pababayaan
+       <br>";
+       echo"Isasayaw kita hanggang sa walang-hanggan
+       <br>";
+       echo" <br>";
+       echo"Mga gunita na laging naiisip (naiisip)
+       <br>";
+       echo"Sumisilip (sumisilip) ang itinakda ng mahiwaga
+       <br>";
+       echo"Liwanag na dulot mo, nagbigay-sinag sa madilim kong mundo
+       <br>";
+       echo"Ibang-iba ako kapag ikaw na ang kapiling
+       <br>";
+       echo"Sumisiping ang buwan at mga bituin
+       <br>";
+       echo"Na para bang sumasang-ayon sa atin ang kalawakan (kalawakan)
+       <br>";
+       echo" <br>";
+       echo"Namumukadkad ang aking ligaya
+       <br>";
+       echo"Sa tuwing ika'y papalapit na
+       <br>";
+       echo"Hawakan mo ang aking kamay
+       <br>";
+       echo" <br>";
+       echo"Oh, Paraluman, ika'y akin nang
+       <br>";
+       echo"Dadalhin sa 'di mo inaasahang paraiso (paraiso)
+       <br>";
+       echo"Palagi kitang aawitan ng Kundiman
+       <br>";
+       echo"'Di magsasawa, 'di ka pababayaan <br>";
+       echo"Isasayaw kita hanggang sa walang-hanggan <br>";
+       echo" <br>";
+       echo"Pa-pa-para-pa-pa, para-pa-pa-para-Paraluman <br>";
+       echo"Pa-pa-para-pa-pa, para-pa-pa-para-Paraluman <br>";
+       echo"Pa-pa-para-pa-pa, para-pa-pa-para-Paraluman <br>";
+       echo"Pa-pa-para-pa-pa, para-pa-pa-para-Paraluman <br>";
+       echo" <br>";
+       echo"Himig ng tadhana
+       <br>";
+       echo"Sa atin ay tumutugma na
+       <br>";
+       echo"Himig ng tadhana
+       <br>";
+       echo"Sa atin ay tumutugma na
+       <br>";
+       echo" <br>";
+       echo"Oh, Paraluman, ika'y akin nang <br>";
+       echo"Dadalhin sa 'di mo inaasahang paraiso <br>";
+       echo"Palagi kitang aawitan ng Kundiman
+       <br>";
+       echo"'Di magsasawa, 'di ka pababayaan <br>";
+       echo"Isasayaw kita, mamahalin kita
+       <br>";
+       echo"Hanggang sa walang-hanggan <br>";
+       
+       
+       
+    ?>
+
+    <button><a href="index.php">Back</a></button>
+
+</body>
+
+   <style>
+       body{
+        text-align: center;
+       }
+       button a{
+        text-decoration: none;
+        border-radius: 5px;
+       }
+   </style>
+</html>
