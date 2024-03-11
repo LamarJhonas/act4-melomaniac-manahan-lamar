03e1f9db37cfb2d3b85a26c65f4fefd18d1af9a8
Jhonas Lamar <jhonaslamarck6@gmail.com>
2024-03-11 18:26:23 +0800

    song 5 commited by Jhonas Lamar

---
+++ song5.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Raindrops</title>
+</head>
+<body>
+    <h2>Raindrops</h2>
+    <h4>Artist: Ariana Grande</h4>
+
+    <?php
+       echo" Lyrics: <br><br>";
+       echo"When raindrops fell down from the sky
+       <br>";
+       echo"The day you left me, an angel cried
+       <br>";
+       echo"Oh, she cried, an angel cried
+       <br>";
+       echo"She cried
+       <br>";
+       
+       
+    ?>
+
+    <button><a href="index.php">Back</a></button>
+
+</body>
+
+   <style>
+       body{
+        text-align: center;
+       }
+       button a{
+        text-decoration: none;
+        border-radius: 5px;
+       }
+   </style>
+</html>
